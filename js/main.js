4fcae68d1a74b11f4ec18a20b652b396da862836
Mark Viggiano <markcviggiano@gmail.com>
2019-12-06 21:55:26 -0500

    Animal Rework & Lake Added
    Rabbit class was modified to fit all animals to make development easier. Lake class was setup.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 const environment = document.getElementById("mainEnvironment").getContext("2d");
+var organisms = [];
 
-class Rabbit {
-  constructor(age, hunger, thirst, health, rUrge) {
+class Lake {
+  constructor(pollution) {
+    this.pollution = pollution;
+  }
+
+  pollute(lake, addedPollutant) {
+    lake.pollution = lake.pollution + addedPollutant;
+  }
+
+  cleanUp(lake, removedPollutant) {
+    lake.pullution = lake.pollution - removedPollutant;
+  }
+}
+
+
+class Animal {
+  constructor(age, hunger, thirst, health, rUrge, type, x, y) {
     //Provided Variables
+    this.x = x;
+    this.y = y;
     this.age = age;
     this.hunger = hunger;
     this.thirst = thirst;
     this.health = health;
     this.rUrge = rUrge;
+    this.type = type;
 
     //Constants
     this.maxHealth = 100;
@@ -15,53 +34,26 @@ class Rabbit {
     this.maxThirst = 100;
     this.maxAge = 10;
     this.maxRUrge = 10;
-  }
 
-  reproduce(rabbitName) {
-    if(this.age >= 3 && this.rUrge >= 4) {
-      if(rabbitName.age >= 3 && rabbitName.rUrge >= 4) {
-        console.log('Reproduction has occured!');
-      } else {
-        console.log('One or more rabbits cannot reproduce!');
-      }
-    } else {
-      console.log('Cannot yet reproduce!');
-    }
+    //foxes only
+    this.attack = 25;
   }
 
 }
 
 
-function generateLakes() {
-  var lakeNumber = Math.floor(Math.random() * 3);
-  coordinates = [];
 
-  for (var i = 0; i < lakeNumber; i++) {
-    //Add 200 to make sure lakes cannot spawn between 0 - 200 pixels
-    var coordinateX = Math.floor(Math.random() * 1400) + 200;
-    var coordinateY = Math.floor(Math.random() * 1400) + 200;
-
-    coordinates.push([coordinateX, coordinateY]);
-  }
-
-  console.log(coordinates);
-
-  coordinates.forEach( function(cords) {
-    console.log(`A lake has spawned at cords ${cords}`);
-  });
-
-}
 
 function setupEnvironment() {
   environment.fillStyle = "#47D83B";
   environment.fillRect(0,0,1600,800);
-  generateLakes()
 }
 
 window.addEventListener('load', (event) => {
   setupEnvironment()
 
-  var rabbitA = new Rabbit(5, 80, 56, 100, 6);
-  var rabbitB = new Rabbit(5, 20, 90, 20, 4);
-  rabbitB.reproduce(rabbitA);
+
+
+
+
 });
